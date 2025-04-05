f6d775a67aff944959c57abb8bbd0d6b0e6e5309
codefreak6996 <gopalpro5811j@gmail.com>
2025-04-05 11:58:44 +0530

    Add files via upload

---
+++ Output.java
@@ -0,0 +1,21 @@
+Performance Report for Abhishek (Manager):
+Address: UCL Colony
+Salary: $90000.0
+Bonus: $9000.0
+Department: Sales
+Abhishek is managing the project: New Sales Strategy
+
+Performance Report for Harshit (Developer):
+Address: Peptech City
+Salary: $70000.0
+Bonus: $4900.0
+Programming Language: Java
+Harshit is developing the feature: User  Authentication
+
+Performance Report for Gopal (Programmer):
+Address: Gaushala Chowk
+Salary: $60000.0
+Bonus: $4200.0
+Programming Language: Python
+Current Project: Project AP
+Gopal is coding for the project: Project AP
